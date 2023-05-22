62c5d9f5f23a0dfc19db76f99ea24a7602cad3f1
Ryan Milligan <ryanmilligan88@gmail.com>
2023-05-21 23:28:57 -0700

    refactor(main.js): wrap program in main function

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
 import { Application, Graphics } from "pixi.js";
 
-// Create application
-const app = new Application({ width: 640, height: 360 });
-// Append canvas to the body of the DOM
-document.body.appendChild(app.view);
+function main() {
+  // Create application
+  const app = new Application({ width: 640, height: 360 });
+  // Append canvas to the body of the DOM
+  document.body.appendChild(app.view);
 
-// Draw circle
-const graphics = new Graphics();
-graphics.beginFill(0xff0000); // Red
-graphics.lineStyle(10, 0x000000); // Black
-graphics.drawCircle(0, 0, 25);
+  // Draw circle
+  const graphics = new Graphics();
+  graphics.beginFill(0xff0000); // Red
+  graphics.lineStyle(10, 0x000000); // Black
+  graphics.drawCircle(0, 0, 25);
 
-// Add to stage
-app.stage.addChild(graphics);
-graphics.x = app.view.width / 2;
-graphics.y = app.view.height / 2;
+  // Add to stage
+  app.stage.addChild(graphics);
+  graphics.x = app.view.width / 2;
+  graphics.y = app.view.height / 2;
+}
+
+window.addEventListener("load", main);
